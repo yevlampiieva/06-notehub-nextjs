4eb0837f06e52d29d7de6643a358450896c553f4
Alla Yevlampiieva <modan1679@gmail.com>
2025-09-12 16:58:43 +0300

    Add initial files, add notes to app, update layout, add Header, Footer components

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Notes list",
+  description: "Notes list description",
+};
+
+export default function NoteList() {
+  return <div>NotesList page</div>;
+}
